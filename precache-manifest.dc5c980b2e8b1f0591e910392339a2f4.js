df8641e5c92a3810981d6b0a28f10b5bbcd4fe06
jack5972 <meetp6041@gmail.com>
2020-06-09 13:08:50 +0530

    Updates

---
+++ precache-manifest.dc5c980b2e8b1f0591e910392339a2f4.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e010210d38084a2ddef20e9d251746d",
+    "url": "/Nutan/index.html"
+  },
+  {
+    "revision": "7dd7eb4fe405dabb8f2b",
+    "url": "/Nutan/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "92b9adc5e2b77af5185a",
+    "url": "/Nutan/static/css/main.8fc10546.chunk.css"
+  },
+  {
+    "revision": "7dd7eb4fe405dabb8f2b",
+    "url": "/Nutan/static/js/2.43b8e63d.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Nutan/static/js/2.43b8e63d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92b9adc5e2b77af5185a",
+    "url": "/Nutan/static/js/main.9c744628.chunk.js"
+  },
+  {
+    "revision": "7d59d5e86a182413011e",
+    "url": "/Nutan/static/js/runtime-main.bdf1ed8e.js"
+  },
+  {
+    "revision": "9f1d4885bf0135e3a878c5131a272747",
+    "url": "/Nutan/static/media/Nutan_opening.9f1d4885.jpg"
+  },
+  {
+    "revision": "8ef3ee5dd0994b34b0930d445ca195bd",
+    "url": "/Nutan/static/media/about1.8ef3ee5d.jpg"
+  },
+  {
+    "revision": "35936528ed306fbe138c881b6f6eb739",
+    "url": "/Nutan/static/media/blk1.35936528.jpg"
+  },
+  {
+    "revision": "8e62c361c9aeed4d69013ca8a7cd1b89",
+    "url": "/Nutan/static/media/blk2.8e62c361.jpg"
+  },
+  {
+    "revision": "62d43eae929128b28b0a96fca80c7f7d",
+    "url": "/Nutan/static/media/block_banner.62d43eae.jpg"
+  },
+  {
+    "revision": "444c2cbd5be25b7a9bb806527953d9e0",
+    "url": "/Nutan/static/media/conserve.444c2cbd.jpg"
+  },
+  {
+    "revision": "3d832ac40452978d67d20e2277c8cc87",
+    "url": "/Nutan/static/media/gb.3d832ac4.jpg"
+  },
+  {
+    "revision": "80f6f43f31938460e915fc4e7b8c94ca",
+    "url": "/Nutan/static/media/impact.80f6f43f.jpg"
+  },
+  {
+    "revision": "2031a5c6ee3b36223671b863bd397ac3",
+    "url": "/Nutan/static/media/nm.2031a5c6.gif"
+  },
+  {
+    "revision": "5608d1556cf6f0fad307e90f346279d6",
+    "url": "/Nutan/static/media/now_shopping.5608d155.jpg"
+  },
+  {
+    "revision": "8ef3ee5dd0994b34b0930d445ca195bd",
+    "url": "/Nutan/static/media/nutan.8ef3ee5d.jpg"
+  },
+  {
+    "revision": "8e0d9ebed42da449f0d27e322f0c0d5d",
+    "url": "/Nutan/static/media/product_banner_desktop4june.8e0d9ebe.jpg"
+  },
+  {
+    "revision": "09ef117e1e18b80a82f3aac65c45e38d",
+    "url": "/Nutan/static/media/sample.09ef117e.JPG"
+  },
+  {
+    "revision": "9497eed4a0ba45638e215bd55ed3d325",
+    "url": "/Nutan/static/media/ss.9497eed4.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bd1c6dfe2fa75af54f4710acb52b523d.js"
+  "/Nutan/precache-manifest.dc5c980b2e8b1f0591e910392339a2f4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Nutan/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
