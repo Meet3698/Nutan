45bae873a0a0149fa62353c71fffa0ec22666c2e
Shail <raval.shail1@gmail.com>
2020-06-09 10:21:53 +0530

    Updates

---
+++ precache-manifest.fc60e47b08b777d4380c3911a2aa6838.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d720812dcb2d10c42e0710ed4fbdcf3",
+    "url": "/nutan/index.html"
+  },
+  {
+    "revision": "9bdd77c70910f7b4e125",
+    "url": "/nutan/static/css/main.ac741f4d.chunk.css"
+  },
+  {
+    "revision": "be60298ec496ea900a49",
+    "url": "/nutan/static/js/2.d5728fef.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/nutan/static/js/2.d5728fef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bdd77c70910f7b4e125",
+    "url": "/nutan/static/js/main.936b4f3f.chunk.js"
+  },
+  {
+    "revision": "e1299ec98349d03e1b07",
+    "url": "/nutan/static/js/runtime-main.b2d072bf.js"
+  },
+  {
+    "revision": "9f1d4885bf0135e3a878c5131a272747",
+    "url": "/nutan/static/media/Nutan_opening.9f1d4885.jpg"
+  },
+  {
+    "revision": "8ef3ee5dd0994b34b0930d445ca195bd",
+    "url": "/nutan/static/media/about1.8ef3ee5d.jpg"
+  },
+  {
+    "revision": "35936528ed306fbe138c881b6f6eb739",
+    "url": "/nutan/static/media/blk1.35936528.jpg"
+  },
+  {
+    "revision": "8e62c361c9aeed4d69013ca8a7cd1b89",
+    "url": "/nutan/static/media/blk2.8e62c361.jpg"
+  },
+  {
+    "revision": "62d43eae929128b28b0a96fca80c7f7d",
+    "url": "/nutan/static/media/block_banner.62d43eae.jpg"
+  },
+  {
+    "revision": "444c2cbd5be25b7a9bb806527953d9e0",
+    "url": "/nutan/static/media/conserve.444c2cbd.jpg"
+  },
+  {
+    "revision": "f45b436e6c982b5ae8d1e0eceebafa11",
+    "url": "/nutan/static/media/f_faridagupta_Logo.f45b436e.png"
+  },
+  {
+    "revision": "3d832ac40452978d67d20e2277c8cc87",
+    "url": "/nutan/static/media/gb.3d832ac4.jpg"
+  },
+  {
+    "revision": "80f6f43f31938460e915fc4e7b8c94ca",
+    "url": "/nutan/static/media/impact.80f6f43f.jpg"
+  },
+  {
+    "revision": "2031a5c6ee3b36223671b863bd397ac3",
+    "url": "/nutan/static/media/nm.2031a5c6.gif"
+  },
+  {
+    "revision": "5608d1556cf6f0fad307e90f346279d6",
+    "url": "/nutan/static/media/now_shopping.5608d155.jpg"
+  },
+  {
+    "revision": "8ef3ee5dd0994b34b0930d445ca195bd",
+    "url": "/nutan/static/media/nutan.8ef3ee5d.jpg"
+  },
+  {
+    "revision": "8e0d9ebed42da449f0d27e322f0c0d5d",
+    "url": "/nutan/static/media/product_banner_desktop4june.8e0d9ebe.jpg"
+  },
+  {
+    "revision": "09ef117e1e18b80a82f3aac65c45e38d",
+    "url": "/nutan/static/media/sample.09ef117e.JPG"
+  },
+  {
+    "revision": "9497eed4a0ba45638e215bd55ed3d325",
+    "url": "/nutan/static/media/ss.9497eed4.jpg"
+  }
+]);
